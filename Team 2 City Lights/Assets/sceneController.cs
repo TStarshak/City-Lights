4da55d68c0268f20a9b1a01be07ef9ddea0c96b7
George Vo <47114590+Ooki42@users.noreply.github.com>
2019-09-26 06:30:47 -0500

    Created working overlay

---
+++ Team 2 City Lights/Assets/sceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneController : MonoBehaviour
+{
+    public GameObject overlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (overlay.activeInHierarchy)
+            {
+                overlay.SetActive(false);
+
+            }
+
+        }
+    }
+}
