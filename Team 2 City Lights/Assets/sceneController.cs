626cfc180affd1028302554e482c6dfd88f55486
Arik Stewart <ams382@case.edu>
2019-09-29 21:14:16 -0400

    Additional Changes

---
+++ Team 2 City Lights/Assets/sceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneController : MonoBehaviour
+{
+    public GameObject overlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (overlay.activeInHierarchy)
+            {
+                overlay.SetActive(false);
+
+            }
+            else
+            {
+                overlay.SetActive(true);
+            }
+
+        }
+    }
+}
