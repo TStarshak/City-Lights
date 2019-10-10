5e66e9ff6386d5af09f64b1683134263dcba8a87
Sean Rivers <sean.rivers@case.edu>
2019-10-10 08:37:46 -0400

    Code for enemy that fires projectiles

---
+++ Team 2 City Lights/Assets/Scripts/Penemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penemy : MonoBehaviour
+{
+    [SerializeField] private GameObject Projectile;
+    public float secondsBetweenSpawn;
+    public float elapsedTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        //timer for spawn
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > secondsBetweenSpawn)
+        {
+            elapsedTime = 0;
+            Instantiate(Projectile);
+        }
+       
+    }
+
+}
