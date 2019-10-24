e69839ab2d21862c81bf0c75f917aaab8a84cee8
sar172C <sar172@case.edu>
2019-10-24 08:10:06 -0400

    full commit of enemy status

---
+++ team 2 City Lights/Assets/Scripts/CityController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+//Used to control City Management and interactions
+public class CityController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Text buttonPrompt;
+    [SerializeField] private string forestScene;
+    private Transform playerTransform;
+    private float playerPosX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNearCityEdge()){
+            enableCityExitPrompt();
+        }
+        else {
+            disableCityExitPrompt();
+        }
+
+        if (Input.GetButtonDown("Action")){
+            if (isNearCityEdge()){
+                SceneManager.LoadScene(forestScene);
+            }
+        }
+
+        
+    }
+
+    // Determines if the given position is near edge of city
+    bool isNearCityEdge(){
+        playerPosX = playerTransform.position.x;
+        return playerPosX > 24.0f || playerPosX < -24.0f;
+    }
+    
+    // Toggles the display and functionality of city exit
+    void enableCityExitPrompt(){
+        //Display Prompt and eligible to exit city
+            buttonPrompt.gameObject.SetActive(true);
+            buttonPrompt.text = "Press 'E' to enter the forest!";
+    }
+    void disableCityExitPrompt(){
+        buttonPrompt.text = "";
+        buttonPrompt.gameObject.SetActive(false);
+    }
+}
---
+++ team 2 City Lights/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/Movement Input")]
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    public float movementSpeed;
+    private CharacterController _charController;
+    public GameObject pauseMenu;
+   // private float sprintMultiplier;
+   // private bool sprinting;
+   // public int stamina = 100;
+
+    void Start()
+    {
+       // sprinting = false;
+        _charController = GetComponent<CharacterController>();
+        movementSpeed = 10.0f;
+        //sprintMultiplier = 2.0f;
+    }
+
+    void Update()
+    {
+        /* if (Input.GetKey(KeyCode.LeftShift))
+         {
+             StartCoroutine(Sprint());
+         }
+         else
+         {
+             movementSpeed = 10f;
+             sprinting = false;
+         }
+
+         if(Input.GetKeyUp(KeyCode.LeftShift) || stamina == 0)
+             StartCoroutine(RecoverStamina()); */
+        if (pauseMenu.activeInHierarchy == false)
+        {
+            float deltaX = Input.GetAxis("Horizontal") * movementSpeed;
+            float deltaZ = Input.GetAxis("Vertical") * movementSpeed;
+            Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+            movement = Vector3.ClampMagnitude(movement, movementSpeed);
+            movement.y = 0f;
+
+            //Move the character
+            movement *= Time.deltaTime;
+            movement = transform.TransformDirection(movement);
+            _charController.Move(movement);
+        }
+    }
+
+    
+/*IEnumerator Sprint()
+    {
+        if (stamina > 0)
+        {
+            stamina--;
+            if (!sprinting) { 
+                sprinting = true;
+                movementSpeed *= sprintMultiplier;
+            }
+            CameraFollow.speed = 4.0f;
+        }
+        else
+        {
+            movementSpeed = 10f;
+            sprinting = false;
+        }
+
+        yield return null;
+    }
+
+    IEnumerator RecoverStamina()
+    {
+        if (stamina == 0)
+            yield return new WaitForSeconds(1.0f);
+        while (!Input.GetKey(KeyCode.LeftShift) || stamina == 0)
+        {
+            if(stamina < 100)
+                stamina++;
+           yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return null;
+    } */
+
+}
