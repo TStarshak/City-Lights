be81ddf2f442a067fa9ac1d383342a84a33851d8
Arik Stewart <ams382@case.edu>
2019-11-07 03:43:00 -0500

    Working on converting scenecontroller from local to global manager

---
+++ Team 2 City Lights/Assets/Scripts/GlobalControl/SceneController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Global Script for Scene Management and Navigation */
+public class SceneController : MonoBehaviour
+{
+
+    public static SceneController Instance;
+
+    public GameObject overlay;
+    public GameObject overlaySuper;
+    public static bool isPaused = false;
+
+    // Called upon object creation
+    void Awake()
+    {
+        // Instantiate this tracker if none exists yet and keep game object alive
+        if (Instance == null){
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        // Otherwise prevent duplicates
+        else if (Instance != this){
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (overlaySuper.activeInHierarchy)
+            {
+                overlaySuper.SetActive(false);
+            }
+            else
+            {
+                if (overlay.activeInHierarchy)
+                {
+                    overlay.SetActive(false);
+                    isPaused = true;
+                }
+                else
+                {
+                    overlay.SetActive(true);
+                    isPaused = false;
+                }
+            }
+        }
+    }
+}
