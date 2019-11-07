eaaebeb3b5fe6e95dd3188f200fae4f162ab48d6
sar172C <sar172@case.edu>
2019-11-06 22:26:16 -0500

    added additional enemies to master

---
+++ Team 2 City Lights/Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    private float lifeSpan = 2.0f;
+    public Transform pExplosion;
+    public Transform soundExplosion;
+    private float projectileSpeed = 10f;
+    public Transform player;
+
+
+    void Update()
+    {
+        //gets the players transform
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //the direction the projectile will travel
+        Vector3 dir = player.position - transform.position;
+        this.GetComponent<Rigidbody>().velocity = dir.normalized * projectileSpeed;
+        //Destroys the projectile after it has lived its life
+        Destroy(this.gameObject, lifeSpan);
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "Enemy")
+        {
+            if (other.gameObject.tag == "Player") ;
+            {
+                Destroy(this.gameObject);
+
+            }
+        }
+    }
+
+}
---
+++ Team 2 City Lights/Assets/Scripts/SpiderWeb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderWeb : MonoBehaviour
+{
+    GameObject player;
+    PlayerMovement pMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pMove = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pMove.movementSpeed *= .5f;
+        }
+
+    }
+}
