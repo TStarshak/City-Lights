dd31d21748abe133b89668b643214d378cc948c7
Arik Stewart <ams382@case.edu>
2019-11-21 14:33:01 -0500

    Working music slider for Main Menu options

---
+++ Team 2 City Lights/Assets/Scripts/GlobalControl/MusicController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start(){
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+    }
+
+    // Sets the level of the mixer volume to the value of the slider
+    public void setVolume(){
+        float sliderValue = slider.value;
+        // Sets music vol to slider value as a log of base 10 for the slider value
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+}
