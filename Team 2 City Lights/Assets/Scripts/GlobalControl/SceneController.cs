d6d6926d3a62dd11c658c305b2df69ec0525973f
Arik Stewart <ams382@case.edu>
2019-11-23 18:20:53 -0500

    Base Loading Screen Added

--- Team 2 City Lights/Assets/Scripts/GlobalControl/SceneController.cs
+++ Team 2 City Lights/Assets/Scripts/GlobalControl/SceneController.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 /* Global Script for Scene Management and Navigation */
 public class SceneController : MonoBehaviour
 {
-
     public static SceneController Instance;
     public static Scene currentScene; //The Current Loaded Scene
 
@@ -29,15 +28,36 @@ public class SceneController : MonoBehaviour
 
     public static void LoadScene(string scene){
         PlayerState.SavePlayer();
-        SceneManager.LoadScene(scene);
+        Instance.InitLoadingScene(scene);
         currentScene = SceneManager.GetSceneByName(scene);
         if (scene == "MainMenu"){
             Instance.ResetGame();
         }
     }
 
+    public static void LoadFirstScene(){
+        Instance.InitLoadingScene("City");
+        currentScene = SceneManager.GetSceneByName("City");
+    }
+
     // Destroy the game manager and reset the player's current progress
     public void ResetGame(){
         GameObject.Destroy(gameObject);
+    }
+
+    // Displays the loading screen and begins loading the next scene in the background
+    void InitLoadingScene(string nextScene){
+        SceneManager.LoadScene("LoadingScreen");
+        //Start asyncOperation
+        StartCoroutine(LoadAsyncScene(nextScene));
+    }
+
+    IEnumerator LoadAsyncScene(string nextScene){
+        // Create an async operation
+        yield return new WaitForSeconds(3.0f);
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(nextScene);
+        // Begin firefly animation
+        // When finished, load the game scene
+        yield return new WaitForEndOfFrame();
     }
 }
--- Team 2 City Lights/Assets/Scripts/UI/MenuButtonController.cs
+++ Team 2 City Lights/Assets/Scripts/UI/MenuButtonController.cs
@@ -12,7 +12,11 @@ public class MenuButtonController : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        SceneController.LoadScene(sceneName);
+    }
+
+    public void StartGame(){
+        SceneController.LoadFirstScene();
     }
 
     //Quit to main menu and reset progress
