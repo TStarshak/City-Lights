9148d43f3975d17a0e9fab756cd1b49aeed59bb8
Trey Starshak <threestarhouse@gmail.com>
2019-12-09 18:13:12 -0500

    Fixed some merge errors

--- Team 2 City Lights/Assets/Scripts/Penemy.cs
+++ Team 2 City Lights/Assets/Scripts/Penemy.cs
@@ -33,72 +33,65 @@ public class Penemy : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
-<<<<<<< HEAD
         this.transform.rotation = rot;
-        //timer for spawn
         elapsedTime += Time.deltaTime;
 
-        if (elapsedTime > secondsBetweenSpawn && pMesh.enabled) 
-=======
-        if (!PlayerState.localPlayerData.isDead)
->>>>>>> 888758deae04525be13f501ac32bc5d454841849
-        {
-            this.transform.rotation = rot;
-            //timer for spawn
-            elapsedTime += Time.deltaTime;
-
-            if (elapsedTime > secondsBetweenSpawn)
+        if (elapsedTime > secondsBetweenSpawn && pMesh.enabled)
+            if (!PlayerState.localPlayerData.isDead)
             {
-                elapsedTime = 0;
-                Instantiate(Projectile, transform.position, transform.rotation);
-                anim.SetTrigger("Attack");
-            }
+                this.transform.rotation = rot;
+                //timer for spawn
+                elapsedTime += Time.deltaTime;
 
-<<<<<<< HEAD
-        if (distance > activationDistance && pMesh.enabled)
-        {
-            Vector3 dirToPlayer = transform.position - Player.transform.position;
-
-            Vector3 newPos = transform.position - dirToPlayer;
-
-            pMesh.SetDestination(newPos);
-            float xPos = dirToPlayer.x;
-            if (xPos > 0 && lookRight)
-=======
-            float distance = Vector3.Distance(transform.position, Player.transform.position);
-
-            if (distance > activationDistance)
->>>>>>> 888758deae04525be13f501ac32bc5d454841849
-            {
-                Vector3 dirToPlayer = transform.position - Player.transform.position;
-
-                Vector3 newPos = transform.position - dirToPlayer;
-
-                pMesh.SetDestination(newPos);
-                float xPos = dirToPlayer.x;
-                if (xPos > 0 && lookRight)
+                if (elapsedTime > secondsBetweenSpawn)
                 {
-                    lookRight = false;
-                    rend.flipX = false;
-                }
-                else if (xPos < 0 && !lookRight)
-                {
-                    lookRight = true;
-                    rend.flipX = true;
+                    elapsedTime = 0;
+                    Instantiate(Projectile, transform.position, transform.rotation);
+                    anim.SetTrigger("Attack");
                 }
 
-            }
-            else
-            {
-                pMesh.SetDestination(transform.position);
-            }
-<<<<<<< HEAD
+                if (distance > activationDistance && pMesh.enabled)
+                {
+                    Vector3 dirToPlayer = transform.position - Player.transform.position;
 
-        }
-        else if(pMesh.enabled)
-        {
-            pMesh.SetDestination(transform.position);
-        }
+                    Vector3 newPos = transform.position - dirToPlayer;
+
+                    pMesh.SetDestination(newPos);
+                    float xPos = dirToPlayer.x;
+                    if (xPos > 0 && lookRight)
+                        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+                    if (distance > activationDistance)
+                    {
+                        Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+                        Vector3 newPos = transform.position - dirToPlayer;
+
+                        pMesh.SetDestination(newPos);
+                        float xPos = dirToPlayer.x;
+                        if (xPos > 0 && lookRight)
+                        {
+                            lookRight = false;
+                            rend.flipX = false;
+                        }
+                        else if (xPos < 0 && !lookRight)
+                        {
+                            lookRight = true;
+                            rend.flipX = true;
+                        }
+
+                    }
+                    else
+                    {
+                        pMesh.SetDestination(transform.position);
+                    }
+
+                }
+                else if (pMesh.enabled)
+                {
+                    pMesh.SetDestination(transform.position);
+                }
+            }
     }
 
     private IEnumerator shadeSpawn()
@@ -128,10 +121,6 @@ public class Penemy : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-=======
-        }
-    }
->>>>>>> 888758deae04525be13f501ac32bc5d454841849
 }
 
 
